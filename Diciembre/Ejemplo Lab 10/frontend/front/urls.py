8344edea95b9df51df56615856d07aa77a776532
Jackeline Benitez <jackelinebenitez112@gmail.com>
2022-12-27 08:47:28 -0600

    feat: frontend done

---
+++ Diciembre/Ejemplo Lab 10/frontend/front/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.login,name='front-login'),
+    path('login/',views.signin, name='signin'),
+    path('home/', views.home,name='front-home'),
+    path('add/',views.add,name='add'),
+    path('cargar/',views.cargaMasiva,name='carga'),
+]
---
+++ Diciembre/Ejemplo Lab 10/frontend/front/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from .forms import LoginForm,FileForm,AddForm
+
+import requests
+
+servidor = 'http://127.0.0.1:5000/'
+
+def signin(request):
+    contexto={}
+    if request.method=='GET':
+        form=LoginForm(request.GET)
+        if form.is_valid():
+            user=form.cleaned_data['username']
+            passw=form.cleaned_data['password']
+            r = requests.get(servidor+'login/'+user+'/'+passw); #http://127.0.0.1:5000/login/patricio/bobesponja
+            data=r.json()
+            print(data['data'])
+            if data['data']==True:
+                contexto={
+                    'title':'Home'
+                }
+    return render(request,'home.html',contexto)
+
+def login(request):
+    contexto={
+        'title':'Login'
+    }
+    return render(request,'login.html',contexto)
+
+def home(request):
+    contexto={
+        'canciones':[]
+    }
+    try:
+        response=requests.get(servidor+'canciones') #http://127.0.0.1:5000/canciones
+        canciones=response.json()
+        contexto['canciones']=canciones
+    except:
+        print('Error en la API')
+    return render(request,'home.html',contexto)
+
+def add(request):
+    if request.method=='POST':
+        form=AddForm(request.POST)
+        if form.is_valid():
+            json_data=form.cleaned_data
+            response=requests.post(servidor+'agregarCancion',json=json_data)
+            if response.ok:
+                return render(request,'add.html',{'form':form})
+        return render(request,'add.html',{'form':form})
+    return render(request,'add.html')
+
+def cargaMasiva(request):
+    ctx={
+        'content':None,
+        'response':None
+    }
+    if request.method=='POST':
+        form=FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            f=request.FILES['file']
+            xml_binary=f.read()
+            xml=xml_binary.decode('utf-8')
+            ctx['content']=xml
+            response=requests.post(servidor+'agregarCanciones',data=xml_binary)
+            if response.ok:
+                ctx['response']='Archivo XML cargado correctamente'
+            else:
+                ctx['response']='El archivo se envio, pero hubo un error en el servidor'
+    else:
+        return render(request,'carga.html')
+    return render(request,'carga.html',ctx)
